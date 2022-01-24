0c2273289ea602aa5850ba83e78f6153deabdcfa
konien-makiraz <mehmetalikiraz@konien.com>
2022-01-24 18:20:34 +0300

    addon change sale_cancel_reason_add_state > sale_cancel_reason_upgraded

--- sale_cancel_reason_upgraded/__manifest__.py
+++ sale_cancel_reason_upgraded/__manifest__.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Sale Cancel Reason Add State',
+    'name': 'Sale Cancel Reason Upgraded',
     'version': '10.0.1',
     'author': 'Camptocamp, Odoo Community Association',
     'category': 'Sale',
     'license': 'AGPL-3',
     'complexity': 'normal',
     'website': 'http://www.camptocamp.com',
-    'summary': 'Add sale cancellation reason',
+    'summary': 'Sale Cancel Reason Upgraded',
     'description': '''
-        Sale Cancel Reason Add State
-        ==================
+        Sale Cancel Reason Upgraded
         When a sale order is canceled, a reason must be given,
         it is chosen from a configured list. ''',
-    'depends': ['sale', 'sale_cancel_reason','sale_order_cancel_reason'],
+    'depends': ['sale_cancel_reason'],
     'auto_install': False,
     'installable': True,
+    'data': [],
 }
