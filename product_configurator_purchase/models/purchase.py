c21f5458e464ad4798a94ba0df67c3784ce80764
konien-makiraz <mehmetalikiraz@konien.com>
2023-04-18 18:44:32 +0300

    ProductConfigurator_purchase addon

--- product_configurator_purchase/models/purchase.py
+++ product_configurator_purchase/models/purchase.py
@@ -1,5 +1,6 @@
-from odoo import api, fields, models,_
-
+from odoo import api, fields, models,_, SUPERUSER_ID
+from datetime import datetime
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
@@ -57,11 +58,43 @@ class PurchaseOrderLine(models.Model):
         )
 
     @api.onchange('product_id')
-    def product_name_change(self):
-        self.ensure_one()
+    def onchange_product_id(self):
+        result = {}
+        if not self.product_id:
+            return result
+
+        # Reset date, price and quantity since _onchange_quantity will provide default values
+        self.date_planned = datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        self.price_unit = self.product_qty = 0.0
+        self.product_uom = self.product_id.uom_po_id or self.product_id.uom_id
+        result['domain'] = {'product_uom': [('category_id', '=', self.product_id.uom_id.category_id.id)]}
+
+        product_lang = self.product_id.with_context(
+            lang=self.partner_id.lang,
+            partner_id=self.partner_id.id,
+        )
+
         if self.config_ok:
-            name = self.product_id.name + "\n"
-            for attrib_line in self.product_id.attribute_value_ids:
-                name += _(attrib_line.display_name) + "\n"
-            self.with_context({'name': name})
-            #self.name = name
+            product = self.product_id.with_context(
+                lang=self.partner_id.lang).browse(self.product_id.id)
+            convert_name = product.name + "\n"
+            for attrib_line in product.attribute_value_ids:
+                convert_name += _(attrib_line.display_name) + "\n"
+            self.name = convert_name
+        else:
+            self.name = product_lang.display_name
+            if product_lang.description_purchase:
+                self.name += '\n' + product_lang.description_purchase
+
+        fpos = self.order_id.fiscal_position_id
+        if self.env.uid == SUPERUSER_ID:
+            company_id = self.env.user.company_id.id
+            self.taxes_id = fpos.map_tax(
+                self.product_id.supplier_taxes_id.filtered(lambda r: r.company_id.id == company_id))
+        else:
+            self.taxes_id = fpos.map_tax(self.product_id.supplier_taxes_id)
+
+        self._suggest_quantity()
+        self._onchange_quantity()
+
+        return result
